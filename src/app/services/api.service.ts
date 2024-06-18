a9719b2fe1dc5bd4a77a7b777a176540c9971944
Frederic Bec <bec.frederic90@outlook.fr>
2024-06-18 15:58:21 +0200

    Add city request

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Hotel } from '../model/hotel.model';
 import { environment } from 'src/environments/environment';
+import { City } from '../model/city.model';
+import { env } from 'process';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +17,7 @@ export class ApiService {
   }
 
   public getHotelsByCity(id : number){
-    return this.http.get<Hotel[]>(environment.host + "/hotels/city" + id);
+    return this.http.get<Hotel[]>(environment.host + "/hotels/city/" + id);
   }
 
   public getHotel(id : number){
@@ -32,5 +34,25 @@ export class ApiService {
 
   public deleteHotel(id : number){
     return this.http.delete<Hotel>(environment.host + "/hotels/" + id);
+  }
+
+  public getCities(){
+    return this.http.get<City[]>(environment.host + "/cities");
+  }
+
+  public getCityById(id : number){
+    return this.http.get<City>(environment.host + "/cities/" + id);
+  }
+
+  public addNewCity(city : City){
+    return this.http.post<City>(environment.host + "/cities", city);
+  }
+
+  public updateCity(id : number, city : City){
+    
+  }
+
+  public deleteCity(id : number){
+    return this.http.delete<City>(environment.host + "/cities/" + id);
   }
 }
