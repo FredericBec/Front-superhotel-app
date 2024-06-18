05de176c99e1b6a1dc2670f68a33fa9d9dd3e32d
Frederic Bec <bec.frederic90@outlook.fr>
2024-06-18 15:58:57 +0200

    Add city function and nav link

--- src/app/components/hotels/hotels.component.ts
+++ src/app/components/hotels/hotels.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { City } from 'src/app/model/city.model';
 import { Hotel } from 'src/app/model/hotel.model';
 import { ApiService } from 'src/app/services/api.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-hotels',
@@ -14,11 +15,13 @@ export class HotelsComponent implements OnInit {
   listCities : City[] = [];
   error : any;
   starRating : number[] = [1,2,3,4,5];
+  urlHost : string = environment.host;
 
   constructor(private apiService : ApiService) { }
 
   ngOnInit(): void {
     this.getAllHotels();
+    this.getAllCities();
   }
 
   getAllHotels(){
@@ -27,6 +30,22 @@ export class HotelsComponent implements OnInit {
       error : (err) => this.error = err,
       complete : () => this.error = null
     });
+  }
+
+  getAllCities(){
+    this.apiService.getCities().subscribe({
+      next : (data) => this.listCities = data,
+      error : (err) => this.error = err,
+      complete : () => this.error = null
+    })
+  }
+
+  getAllHotelsByCity(city : City){
+    this.apiService.getHotelsByCity(city.id).subscribe({
+      next : (data) => this.listHotels = data,
+      error : (err) => this.error = err,
+      complete : () => this.error = null
+    })
   }
 
 }
