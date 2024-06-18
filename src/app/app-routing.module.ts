db1f5f15d06c52f27afc08f70ade8b0335c90347
Frederic Bec <bec.frederic90@outlook.fr>
2024-06-18 14:32:32 +0200

    Add hotel and 404 route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HotelsComponent } from './components/hotels/hotels.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'hotels', component : HotelsComponent},
+  {path : '', redirectTo : 'hotels', pathMatch : 'full'},
+  {path : '404', component : PageNotFoundComponent},
+  {path : '**', redirectTo : '404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
