f5cf6f3dfc26873007f4af5b080c68dcb1f5ffd6
Frederic Bec <bec.frederic90@outlook.fr>
2024-06-19 12:44:37 +0200

    Add 403 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { HotelsComponent } from './components/hotels/hotels.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { DetailHotelComponent } from './components/detail-hotel/detail-hotel.component';
 import { LoginoutComponent } from './components/loginout/loginout.component';
+import { HotelComponent } from './components/hotel/hotel.component';
+import { AdminGuard } from './guards/admin.guard';
+import { UnauthorizeAccessComponent } from './components/error/unauthorize-access/unauthorize-access.component';
 
 const routes: Routes = [
   {path : 'hotels', component : HotelsComponent},
   {path : 'hotels/:id', component : DetailHotelComponent},
+  {path : 'hotel', component : HotelComponent, canActivate : [AdminGuard]},
   {path : 'login', component : LoginoutComponent},
   {path : '', redirectTo : 'hotels', pathMatch : 'full'},
+  {path : '403', component : UnauthorizeAccessComponent},
   {path : '404', component : PageNotFoundComponent},
   {path : '**', redirectTo : '404'}
 ];
